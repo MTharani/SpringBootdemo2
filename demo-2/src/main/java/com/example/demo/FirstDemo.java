4f9093847243981122bebf625cb885b05019c51b
Thara <Thara@Thara-PC>
2022-05-09 20:33:04 +0530

    Second commit

---
+++ demo-2/src/main/java/com/example/demo/FirstDemo.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FirstDemo 
+{
+  public void display()
+  {
+	  System.out.println("Hello Spring Boot");
+  }
+}
